902aaf2cfedf007cba664d5b7e055f9f998fe37e
sillu24 <56128459+sillu24@users.noreply.github.com>
2023-01-22 22:34:56 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -10,7 +10,7 @@ import pickle
 
 import streamlit as st
 
-loaded_model=pickle.load(open('trained_model.sav','rb')) 
+loaded_model=pickle.load(open('./trained_model.sav','rb')) 
 
 #cretaing a function for prediction
 def ckd_prediction(input_data):
